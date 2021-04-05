831939bfa15dc1bf3f88da4dab2eb609dbae92bd
STP <sportano@gmail.com>
2021-04-05 06:40:21 -0500

    adjusted background format to be block

--- formats/background.js
+++ formats/background.js
@@ -2,10 +2,10 @@ import Parchment from 'parchment';
 import { ColorAttributor } from './color';
 
 let BackgroundClass = new Parchment.Attributor.Class('background', 'ql-bg', {
-  scope: Parchment.Scope.INLINE
+  scope: Parchment.Scope.BLOCK
 });
 let BackgroundStyle = new ColorAttributor('background', 'background-color', {
-  scope: Parchment.Scope.INLINE
+  scope: Parchment.Scope.BLOCK
 });
 
 export { BackgroundClass, BackgroundStyle };
